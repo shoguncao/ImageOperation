5ad58cfe07fa5b7fc8ddeb38bccd5214fe54756b
shoguncao <shoguncao@tencent.com>
2021-08-03 11:28:20 +0800

    增加注释

--- ImageOperation/ImageOperation/ImageOperation.h
+++ ImageOperation/ImageOperation/ImageOperation.h
@@ -6,6 +6,13 @@ class ImageOperation {
 public:
     ImageOperation();
     
+    /// 将RGB均值高于某个值的像素全部调整为指定数值
+    /// @param img 图片
+    /// @param highestPixel 高于此值的像素全部转换到defaultPixel值
     NSImage *removeHighPixel(NSImage *img, uint8_t highestPixel, uint8_t defaultPixel=255);
+    
+    /// 保存图片到指定文件路径
+    /// @param img 图片
+    /// @param filePath 文件路径
     void saveImage(NSImage *img, NSString *filePath);
 };
